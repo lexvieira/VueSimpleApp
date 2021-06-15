ed390b5d06dcf493187925754a2ea29d5c0cf6b7
lexvieira <lexvieira.info@gmail.com>
2021-06-15 20:15:30 +0700

    Final Commit, solving importing issues

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,16 +1,6 @@
-import VueRouter, { Route } from 'vue-router'/*  */
-//Check vue import conflicts - Main.ts App.vue
-
-
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-}
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $router: VueRouter
-  }
 }
