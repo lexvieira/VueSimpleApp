ae1939960fb4b9a0e0a0c6e9fd5c6dc08044d056
lexvieira <lexvieira.info@gmail.com>
2021-06-15 11:52:15 +0700

    Add Routes and Templates, check erros

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,16 @@
+import VueRouter, { Route } from 'vue-router'/*  */
+//Check vue import conflicts - Main.ts App.vue
+
+
 /* eslint-disable */
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $router: VueRouter
+  }
 }
